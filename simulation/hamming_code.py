61957d4dc60ad53cf46017fa42beb6e59addf543
Damian0401 <szkola_abc@o2.pl>
2022-04-09 22:28:37 +0200

    Added hamming decode implementation

--- simulation/hamming_code.py
+++ simulation/hamming_code.py
@@ -15,9 +15,18 @@ def code_hamming(input: list) -> list:
     return output
 
 # Decode input bits
-def decode_hamming():
-    pass
+def decode_hamming(input: list):
+    output = []
 
+    if len(input) % 7 != 0:
+        raise ValueError()
+
+    for i in range(0, len(input), 7):
+        output.extend(correct_error(input[i:i+7]))
+
+    return output
+
+# Code single 4 bit word
 def code_word(input: list) -> list:
     if len(input) != 4:
         raise ValueError()
@@ -29,3 +38,43 @@ def code_word(input: list) -> list:
     code = code % 2
 
     return code[0]
+
+# Calculate syndrome
+def calculate_syndrome(input: list) -> list:
+    if len(input) != 7:
+        raise ValueError()
+
+    inputMatrix = numpy.array([input])
+    H = numpy.array([[1,1,1],[1,0,1],[1,1,0],[0,1,1],[1,0,0],[0,1,0],[0,0,1]])
+
+    syndrome = inputMatrix.dot(H)
+    syndrome = syndrome % 2
+
+    return syndrome[0]
+
+# Check index of the error if exists
+def check_error_index(syndrome: list) -> int:
+    if len(syndrome) != 3:
+        raise ValueError()
+
+    H = [[1,1,1],[1,0,1],[1,1,0],[0,1,1],[1,0,0],[0,1,0],[0,0,1]]
+
+    for index, row in enumerate(H):
+        if syndrome[0] == row[0] and syndrome[1] == row[1] and syndrome[2] == row[2]:
+            return index
+
+    return -1
+
+# Find error and correct it if exists
+def correct_error(input: list) -> list:
+    if len(input) != 7:
+        raise ValueError()
+
+    syndrome = calculate_syndrome(input)
+
+    error_index = check_error_index(syndrome)
+
+    if error_index != -1:
+        input[error_index] = (input[error_index] + 1) % 2
+
+    return input[0:4]
--- simulation/main.py
+++ simulation/main.py
@@ -1,9 +1,23 @@
-from hamming_code import code_hamming
+from hamming_code import *
 
 
 def main():
-    word = [1,0,1,0,1,0,1,0]
-    print(code_hamming(word))
+    # Raw data
+    word = [0,0,0,1,1,0,0,0]
+    print(f'data: {word}')
+
+    # Encode data using hamming algorithm 
+    word_encoded = code_hamming(word)
+    print(f'encoded data: {word_encoded}')
+
+    # Create 2 errora
+    word_encoded[1] = (word_encoded[1] + 1) % 2
+    word_encoded[10] = (word_encoded[10] + 1) % 2
+    print(f'encoded data with error: {word_encoded}')
+
+    # Decode data & correct errors
+    word_decoded = decode_hamming(word_encoded)
+    print(f'data: {word_decoded}')
 
 if __name__ == '__main__':
     main()
